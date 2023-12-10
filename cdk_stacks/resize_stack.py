fa4fa179d8a1515f40c4936e57d418ac4e3e32e9
snowsheep <71830342+sheep-snow@users.noreply.github.com>
2023-12-10 16:27:37 +0900

    snnipets

--- cdk_stacks/resize_stack.py
+++ cdk_stacks/resize_stack.py
@@ -1,18 +1,92 @@
 # from aws_cdk import aws_apigateway as apigw
 from aws_cdk import Duration, Stack, aws_iam
 from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_lambda_event_sources as lambda_events
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_s3_notifications as s3n
+from aws_cdk import aws_sns as sns
+from aws_cdk import aws_sns_subscriptions as sns_subs
+from aws_cdk import aws_sqs as sqs
 from constructs import Construct
 
 
 class IrodollyResizeStack(Stack):
-    """_summary_
+    """CDK Stack
 
-    Args:
-        Stack (_type_): _description_
+    See:
+        https://github.com/aws-samples/aws-cdk-examples/blob/master/python/s3-sns-sqs-lambda-chain/s3_sns_sqs_lambda_chain/s3_sns_sqs_lambda_chain_stack.py
     """
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        LAMBDA_TIMEOUT = 60
+
+        # 処理失敗時の DLQ (Optional)
+        dlq = sqs.Queue(
+            self, id="dead_letter_queue_id", retention_period=Duration.days(7)
+        )
+        dead_letter_queue = sqs.DeadLetterQueue(max_receive_count=1, queue=dlq)
+
+        # S3 アップロードイベント
+        upload_queue = sqs.Queue(
+            self,
+            id="sample_queue_id",
+            dead_letter_queue=dead_letter_queue,
+            visibility_timeout=Duration.seconds(LAMBDA_TIMEOUT * 6),
+        )
+        sqs_subscription = sns_subs.SqsSubscription(
+            upload_queue, raw_message_delivery=True
+        )
+        upload_event_topic = sns.Topic(self, id="sample_sns_topic_id")
+        # This binds the SNS Topic to the SQS Queue
+        upload_event_topic.add_subscription(sqs_subscription)
+
+        # S3 Bucket
+        lifecycle_rules = [
+            s3.LifecycleRule(
+                enabled=True,
+                expiration=Duration.days(90),
+                transitions=[
+                    s3.Transition(
+                        storage_class=s3.StorageClass.INFREQUENT_ACCESS,
+                        transition_after=Duration.days(5),
+                    ),
+                    s3.Transition(
+                        storage_class=s3.StorageClass.GLACIER,
+                        transition_after=Duration.days(30),
+                    ),
+                ],
+            )
+        ]
+        s3_bucket = s3.Bucket(
+            self,
+            id=f"{__name__}-sample-bucket",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            versioned=True,
+            lifecycle_rules=lifecycle_rules,
+        )
+
+        # Note: If you don't specify a filter all uploads will trigger an event.
+        #       Also, modifying the event type will handle other object operations
+        # This binds the S3 bucket to the SNS Topic
+        s3_bucket.add_event_notification(
+            s3.EventType.OBJECT_CREATED_PUT,
+            s3n.SnsDestination(upload_event_topic),
+            s3.NotificationKeyFilter(prefix="uploads", suffix=".csv"),
+        )
+        function = _lambda.Function(
+            self,
+            "lambda_function",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="lambda_function.handler",
+            code=_lambda.Code.from_asset(path=lambda_dir),
+            timeout=Duration.seconds(LAMBDA_TIMEOUT),
+        )
+
+        # This binds the lambda to the SQS Queue
+        invoke_event_source = lambda_events.SqsEventSource(upload_queue)
+        function.add_event_source(invoke_event_source)
 
         resize_lambda_role = aws_iam.Role(
             self,
@@ -23,6 +97,8 @@ class IrodollyResizeStack(Stack):
         resize_lambda_role.add_managed_policy(
             aws_iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambdaExecute")
         )
+
+        upload_event_topic = sns.Topic(self, id="sample_sns_topic_id")
 
         # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_lambda/DockerImageFunction.html
         resize_lambda = _lambda.DockerImageFunction(
--- src/resize.py
+++ src/resize.py
@@ -1,17 +1,45 @@
 import json
+import tempfile
+from pathlib import PurePosixPath
+from urllib.parse import unquote_plus
+from uuid import uuid4
+
+import boto3
+import cv2
 
 from libs.log import get_logger
-from
 
 log = get_logger(__name__)
+s3 = boto3.client("s3")
 
 
-def handler(event, context):
+def handler(event, _):
     """Resizes uploaded image"""
     event_json_string = json.dumps(event)
     log.info(f"request: {event_json_string}")
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/plain"},
-        "body": f"You have hit {event_json_string}",
-    }
+
+    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+    key = unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
+    bucket = s3.Bucket(bucket_name)
+    log.info(f"Inputted Image: {bucket_name}/{key}")
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        save_path = PurePosixPath(tmpdir).joinpath(key)
+        log.info(tmpdir)
+        # downloads target object
+        with save_path.open("wb") as f:
+            bucket.download_fileobj(key, f)
+
+        org_image = cv2.imread(save_path)
+        gray_image = cv2.cvtColor(org_image, cv2.COLOR_RGB2GRAY)
+        processed_obj_name = uuid4()
+        suffix = save_path.suffix
+        processed_image = (
+            PurePosixPath(tmpdir).joinpath(processed_obj_name).joinpath(suffix)
+        )
+        cv2.imwrite(processed_image, gray_image)
+        s3.upload_file(
+            Filename=processed_image, Bucket=bucket, Key=f"grayed/{processed_image}"
+        )
+
+    return {"statusCode": 200, "headers": {"Content-Type": "text/plain"}}
